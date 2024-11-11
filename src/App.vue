92cd9af2dcbf547316d097e0d2b2a1bb3ec132e1
GeorgyAl <georganer02@gmail.com>
2024-11-11 12:24:02 +0300

    Swiper

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<Swiper></Swiper>
+</template>
+<script >
+import Swiper from './Swiper.vue';
+export default {
+  components: {
+    Swiper
+  },
+
+};
+</script>
+<style src="./style.css">
+
+</style>
---
+++ src/Swiper.vue
@@ -0,0 +1,34 @@
+<template>
+    <main>
+        <div class="wrapper">
+            <swiper :navigation="true" :modules="modules">
+                <swiper-slide><img src="./img/slide2.jpg" alt="err"></swiper-slide>
+                <swiper-slide><img src="./img/slide3.jpg" alt="err"></swiper-slide>
+                <swiper-slide><img src="./img/slide4.jpg" alt="err"></swiper-slide>
+            </swiper>
+        </div>
+    </main>
+</template>
+<script >
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+
+import 'swiper/css';
+
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Navigation],
+    };
+  },
+};
+</script>
+<style src="./style.css">
+
+</style>
